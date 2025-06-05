a0471b5dfb1a9bc7322e8ef372a90850cbbadf32
oussama-fiali <oussama.halima-filali@laplateforme.io>
2025-06-05 18:48:14 +0200

    reprise du projet a zero /changement d'architecture du projet

--- backend/api/promotions/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../config/database.php';
-
-$db = (new Database())->connect();
-$stmt = $db->query("SELECT * FROM promotions WHERE NOW() BETWEEN date_debut AND date_fin");
-$promos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($promos);
-?>
